5ace3a2f454c4ba4ac11bd8542c0e6fc79d9625a
aifengxx <1207298843@qq.com>
2019-01-31 21:31:08 +0800

    add saveFileName and show how to save a file

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,7 @@ MainWindow::MainWindow(QWidget *parent) :
     this->setWindowTitle("Untitiled_aifengxx.txt");
     QObject::connect(ui->newAction,SIGNAL(triggered()),this,SLOT(newFileSlot()));
     QObject::connect(ui->openAction,SIGNAL(triggered()),this,SLOT(openFlieSlot()));
+    QObject::connect(ui->saveAction,SIGNAL(triggered()),this,SLOT(saveFileSlot()));
 }
 
 MainWindow::~MainWindow()
@@ -36,4 +37,31 @@ void MainWindow::openFlieSlot()
     QString fileName=QFileDialog::getOpenFileName(this,"Open File",QDir::currentPath());
     //这个函数有三个参数：this，“Open File”和QDir。目的是在程序运行目录下的路径下打开
     qDebug()<<"open file"<<fileName;
+}
+void MainWindow::saveFileSlot()
+{
+    QString fileName=QFileDialog::getSaveFileName(this,"Save File",QDir::currentPath());
+    //这样无论如何都会跳出窗口，这是个bug
+    if(fileName.isEmpty())
+    {
+        QMessageBox::information(this,"error","Please enter a file name");
+        return;
+    }
+    QFile *file=new QFile;
+    file->setFileName(fileName);
+    bool ok=file->open(QIODevice::WriteOnly);//写入文件？
+    if(ok)
+    {
+        QTextStream out(file);
+        //ui->textEdit->toPlainText();//这里是取出textEdit当中的纯文本
+        out<<ui->textEdit->toPlainText();//我们做一个重对象，放到out当中。这个流当中就放到C盘当中去了
+        file->close();//保存文件后关闭文件
+        delete file;//然后删除文件
+        //这样我们就演示了怎么保存文件，尽管有些抽象和不理解内部的机制
+    }
+    else
+    {
+        QMessageBox::information(this,"Error","Save Error");
+        return;
+    }
 }
